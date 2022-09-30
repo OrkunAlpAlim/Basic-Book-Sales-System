d04e8fc6f70c2e832fe124e3078941525c039958
orkunalpalim <106318587+OrkunAlpAlim@users.noreply.github.com>
2022-09-30 20:09:24 +0300

    Initial Commit

---
+++ Basic Book Sales System/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Basic Book Sales System
+//
+//  Created by Orkun Alp Alim on 30.09.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
